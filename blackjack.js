4269989b8735034b5499f7274716601b863deb5e
Bobby Neelon <robert.neelon@gmail.com>
2018-04-20 19:36:21 +0000

    hitMe

--- blackjack.js
+++ blackjack.js
@@ -2,8 +2,11 @@
       $('#deal-button').click(function(){
           $('#dealer-hand').append('<img src="img/PNG/2C.png" class="card">');
           $('#dealer-hand').append('<img src="img/PNG/3H.png" class="card">');
-          $('#player-hand').append('<img src="img/PNG/KC.png" class="card">');
+          $('#player-hand').append('<img src="img/PNG/QC.png" class="card">');
           $('#player-hand').append('<img src="img/PNG/JH.png" class="card">');
+      });
+      $('#hit-button').click(function(){
+          $('#player-hand').append('<img src="img/PNG/AH.png" class="card">');
       });
       
     });
