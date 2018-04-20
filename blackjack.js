ddd8c6418b2c91eadc19c7a8e3c7142468b5f68c
Bobby Neelon <robert.neelon@gmail.com>
2018-04-20 19:26:13 +0000

    deal cards

---
+++ blackjack.js
@@ -0,0 +1,9 @@
+    $(function () {
+      $('#deal-button').click(function(){
+          $('#dealer-hand').append('<img src="img/PNG/2C.png" class="card">');
+          $('#dealer-hand').append('<img src="img/PNG/3H.png" class="card">');
+          $('#player-hand').append('<img src="img/PNG/KC.png" class="card">');
+          $('#player-hand').append('<img src="img/PNG/JH.png" class="card">');
+      });
+      
+    });
